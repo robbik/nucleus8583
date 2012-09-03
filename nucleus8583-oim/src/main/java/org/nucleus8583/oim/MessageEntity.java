3e6d9675e44cd1798bde4522f2bd36266e7a145c
Robbi Kurniawan <robbi.kurniawan@gmail.com>
2012-09-03 16:37:53 +0700

    Add which field information if read/write has been failed

--- nucleus8583-oim/src/main/java/org/nucleus8583/oim/MessageEntity.java
+++ nucleus8583-oim/src/main/java/org/nucleus8583/oim/MessageEntity.java
@@ -13,71 +13,87 @@ import rk.commons.util.FastStringWriter;
 
 public final class MessageEntity {
 
-	private Field[] fields;
+    private Field[] fields;
 
-	private int count;
+    private int count;
 
-	public void setFields(List<Field> fields) {
-		count = fields.size();
+    public void setFields(List<Field> fields) {
+        count = fields.size();
 
-		this.fields = fields.toArray(new Field[0]);
-	}
-	
-	public void persist(Message isoMsg, Map<String, Object> root) throws Exception {
-		FastStringWriter sw = new FastStringWriter(1024);
-		FastByteArrayOutputStream baos = new FastByteArrayOutputStream(1024);
+        this.fields = fields.toArray(new Field[0]);
+    }
+    
+    public void persist(Message isoMsg, Map<String, Object> root) throws Exception {
+        FastStringWriter sw = new FastStringWriter(1024);
+        FastByteArrayOutputStream baos = new FastByteArrayOutputStream(1024);
 
-		for (int i = 0; i < count; ++i) {
-			Field f = fields[i];
-			
-			if (f.supportWriter()) {
-				// persist the field
-				f.write(sw, root);
+        for (int i = 0; i < count; ++i) {
+            Field f = fields[i];
+            
+            if (f.supportWriter()) {
+                // persist the field
+                try {
+                    f.write(sw, root);
+                } catch (Throwable t) {
+                   throw new Exception("unable to write field #" + f.getNo(), t);
+                }
 
-				// set to iso-message
-				isoMsg.set(f.getNo(), sw.toString());
+                // set to iso-message
+                isoMsg.set(f.getNo(), sw.toString());
 
-				// reset for later reuse
-				sw.reset();
-			} else {
-				// persist the field
-				f.write(baos, root);
+                // reset for later reuse
+                sw.reset();
+            } else {
+                // persist the field
+                try {
+                    f.write(baos, root);
+                } catch (Throwable t) {
+                   throw new Exception("unable to write field #" + f.getNo(), t);
+                }
 
-				// set to iso-message
-				isoMsg.set(f.getNo(), baos.toByteArray());
+                // set to iso-message
+                isoMsg.set(f.getNo(), baos.toByteArray());
 
-				// reset for later reuse
-				baos.reset();
-			}
-		}
-	}
-	
-	public void load(Message isoMsg, Map<String, Object> root) throws Exception {
-		FastStringReader reader = new FastStringReader();
-		FastByteArrayInputStream inputstream = new FastByteArrayInputStream();
-		
-		for (int i = 0; i < count; ++i) {
-			Field f = fields[i];
-			
-			// get from iso-message
-			Object v = isoMsg.get(f.getNo());
-			
-			if (v == null) {
-				// value is not set, skipping
-			} else if (v instanceof String) {
-				reader.reset((String) v);
-				
-				// load the field
-				f.read(reader, root);
-			} else if (v instanceof byte[]) {
-				inputstream.reset((byte[]) v);
-				
-				// load the field
-				f.read(inputstream, root);
-			} else {
-				throw new UnsupportedOperationException("unable to persist field #" + f.getNo() +
-						" because the field value is not string nor byte[], it is " + v.getClass());
-			}
-		}
-	}
+                // reset for later reuse
+                baos.reset();
+            }
+        }
+    }
+    
+    public void load(Message isoMsg, Map<String, Object> root) throws Exception {
+        FastStringReader reader = new FastStringReader();
+        FastByteArrayInputStream inputstream = new FastByteArrayInputStream();
+        
+        for (int i = 0; i < count; ++i) {
+            Field f = fields[i];
+            
+            // get from iso-message
+            Object v = isoMsg.get(f.getNo());
+            
+            if (v == null) {
+                // value is not set, skipping
+            } else if (v instanceof String) {
+                reader.reset((String) v);
+                
+                // load the field
+                try {
+                   f.read(reader, root);
+                } catch (Throwable t) {
+                   throw new Exception("unable to read field #" + f.getNo(), t);
+                }
+            } else if (v instanceof byte[]) {
+                inputstream.reset((byte[]) v);
+                
+                // load the field
+                try {
+                    f.read(inputstream, root);
+                } catch (Throwable t) {
+                   throw new Exception("unable to read field #" + f.getNo(), t);
+                }
+            } else {
+                throw new UnsupportedOperationException("unable to persist field #" + f.getNo() +
+                        " because the field value is not string nor byte[], it is " + v.getClass());
+            }
+        }
+    }
 }
